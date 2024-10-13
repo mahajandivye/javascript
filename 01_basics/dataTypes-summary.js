996d8c35e96b90a70a15b6b6cc696ed8781805e2
mahajandivye <mahajandivye@gmail.com>
2024-10-13 11:07:06 +0000

    done

---
+++ 01_basics/dataTypes-summary.js
@@ -0,0 +1,41 @@
+// Primitive
+
+// 7 Types: String , Number , Boolean , null , undefined , Symbol , BigInt
+
+// Reference type(Non primitive)
+
+// Array , Objects , Functions
+
+// Javascript is dynamically typed language
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+
+const outsideTemp = null //object
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+const bigNumber = 3456437665443
+
+// console.log(id == anotherId);
+// console.log(typeof id); //symbol and not Symbol
+
+const heroes = ["shaktiman" , "naagraj" , "doga"]
+let myObj = {
+    name: "divye",
+    age: 18,
+}
+
+const myFunction = function()
+{
+    console.log("hello world");
+}
+
+// console.log(typeof myFunction);
+// console.log(typeof heroes);
+
+console.log(typeof myObj);
+
