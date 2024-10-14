598aa00410d1ca32209e3026776de53171c1b578
mahajandivye <mahajandivye@gmail.com>
2024-10-14 11:21:42 +0000

    done

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,47 @@
+const score = 400
+
+const balance = new Number(100)
+
+// console.log(balance);
+// console.log(score);
+
+// console.log(balance.toString());
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+// console.log(balance);
+
+// const otherNumber = 123.8966
+// console.log(otherNumber.toPrecision(3));
+
+// const hundereds = 1000000
+// console.log(hundereds.toLocaleString('en-IN'));
+
+// const store = Number.MAX_VALUE
+
+// console.log(store);
+
+// console.log(Number.MIN_VALUE);
+
+// ++++++++++++++++++++++ Maths ++++++++++++++++++++++++
+
+// console.log(Math);
+
+// console.log(Math.abs(-5));
+// console.log(Math.abs(5));
+// console.log(Math.round(4.6));
+
+// console.log(Math.round(6.5));
+
+// console.log(Math.ceil(6.3));
+
+// console.log(Math.floor(6.8));
+// console.log(Math.min(2 , -100));
+// console.log(Math.max(1,2,3,4));
+
+console.log(Math.random()); // value always between 0 and 1 and decimal value (random function in javascript)
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
