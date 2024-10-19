176c869936830e8196cae8aed8235ee83118e4ef
mahajandivye <mahajandivye@gmail.com>
2024-10-19 18:18:51 +0000

    done

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -51,6 +51,38 @@ function loginUserMessage(username = "sam")
 
 // console.log(loginUserMessage("divye"));
 
-console.log(loginUserMessage());
+// console.log(loginUserMessage());
 
+function calculateCartPrice(val1 , val2 , ...num1)
+{
+    return num1;
+}
 
+console.log(calculateCartPrice(200 , 400 , 500 ,2000));
+
+const user = 
+{
+    username: "divye",
+    price: 199
+}
+
+function handleObject(anyObject)
+{
+    console.log(`username is ${anyObject.username} ans price is ${anyObject.price}`);
+}
+
+// handleObject(user);
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+const myNewArray = [200 , 400 , 100 , 600];
+
+function returnSecondValue(getArray)
+{
+    return getArray[1];
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200 , 400 , 500 , 100 , 600]));
