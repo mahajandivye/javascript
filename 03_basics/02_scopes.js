43db8df8fbeba52d96ea8e970b76c17252fe6d3f
mahajandivye <mahajandivye@gmail.com>
2024-10-21 05:39:16 +0000

    done

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -4,15 +4,50 @@ if(true)
 {
     let a  = 10
     const b = 20
-    console.log(`INNER: ${a}`);
-}
-
-for (let i = 0; i < array.length; i++) {
-    const element = array[i];
-    
+    // console.log(`INNER: ${a}`);
 }
 
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+function one()
+{
+    const username = "divye"
+
+    function two()
+    {
+        const website = "youtube"
+        console.log(username)
+    }
+    // console.log(website)
+
+    two();
+}
+
+// one();
+
+if (true) {
+    const username = "divye"
+    if(username == "divye")
+    {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+// +++++++++++++++++++ interesting ++++++++++++++
+console.log(addone(5))
+function addone(num)
+{
+    return num + 1
+}
+addtwo(5)
+const addtwo = function(num)
+{
+    return num + 2
+}
